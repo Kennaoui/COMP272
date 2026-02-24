d57cf90f9a861cf7271bdce660ff5e96ffe6fb45
Kennaoui <kennaoui@luc.edu>
2026-02-23 20:28:19 -0600

    Update RedBlackTree.java

--- Trees/RedBlackTree.java
+++ Trees/RedBlackTree.java
@@ -152,49 +152,11 @@ public class RedBlackTree {
     // -----------------------
 
     private Node rotateLeft(Node x) {
-        Node y = x.right;
-        if (y == null)
-            return x;
-
-        x.right = y.left;
-        if (y.left != null)
-            y.left.parent = x;
-
-        y.parent = x.parent;
-
-        if (x.parent == null)
-            root = y;
-        else if (x == x.parent.left)
-            x.parent.left = y;
-        else
-            x.parent.right = y;
-
-        y.left = x;
-        x.parent = y;
-        return y;
+        return x;
     }
 
     private Node rotateRight(Node x) {
-        Node y = x.left;
-        if (y == null)
-            return x;
-
-        x.left = y.right;
-        if (y.right != null)
-            y.right.parent = x;
-
-        y.parent = x.parent;
-
-        if (x.parent == null)
-            root = y;
-        else if (x == x.parent.left)
-            x.parent.left = y;
-        else
-            x.parent.right = y;
-
-        y.right = x;
-        x.parent = y;
-        return y;
+        return x;
     }
 
     // -----------------------
