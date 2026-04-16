4d23418904c33d72fe79e7c518d01a1fbd25fb05
Kennaoui <kennaoui@luc.edu>
2026-04-16 12:37:10 -0500

    Create GraphAdjListV2.java

---
+++ Graphs/GraphAdjListV2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Vertex {
+    String label;
+    List<Edge> adjacentEdges;
+
+    public Vertex(String label) {
+        this.label = label;
+        this.adjacentEdges = new ArrayList<>();
+    }
+}
+
+class Edge {
+    Vertex from;
+    Vertex to;
+
+    public Edge(Vertex from, Vertex to) {
+        this.from = from;
+        this.to = to;
+    }
+}
+
+class GraphAdjList implements graph {
+    List<Vertex> vertices;
+    List<Edge> edges; // optional, but often kept
+
+    public Graph() {
+        vertices = new ArrayList<>();
+        edges = new ArrayList<>();
+    }
+}
